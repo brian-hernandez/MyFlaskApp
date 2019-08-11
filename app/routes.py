0d003c46a692071923738a9072a3d834b8f984b7
Brian-Hernandez <mad_cap111@mac.com>
2019-08-10 20:12:22 -0700

    Added a config file, template files and updated the routes file '

--- app/routes.py
+++ app/routes.py
@@ -1,15 +1,16 @@
 from flask import render_template
 from app import app
+from app.forms import LoginForm
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username' : 'Brian'}
+    user = {'username': 'Brian'}
     posts = [
         {
-            'author' : {'username' : 'Brian'},
-            'body' : 'I am living in Cupertino for the Summer.',
-            'times' : [0, 1, 2, 3, 4, 5, 6, 7]
+            'author': {'username': 'Brian'},
+            'body': 'I am living in Cupertino for the Summer.'
         },
         {
             'author': {'username': 'Rascal'},
@@ -17,3 +18,9 @@ def index():
         }
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
+
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Sign In', form=form)
